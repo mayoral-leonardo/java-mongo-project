27086f47adbf719f2467da5beffa4f1a9c5b22ab
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2023-11-19 16:15:40 -0300

    feat: add create user route

--- src/main/java/com/mayoral/javamongo/services/UserService.java
+++ src/main/java/com/mayoral/javamongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mayoral.javamongo.domain.User;
+import com.mayoral.javamongo.dto.UserDTO;
 import com.mayoral.javamongo.repository.UserRepository;
 import com.mayoral.javamongo.services.exception.ObjectNotFoundException;
 
@@ -19,9 +20,17 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
